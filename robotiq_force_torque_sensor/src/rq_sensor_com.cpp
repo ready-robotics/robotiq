37c86d3e959aaf73638a1b17366942f74270de18
Adam Petrone <adam@ready-robotics.com>
2017-06-09 15:22:07 -0400

    Close connection on exceptions

--- robotiq_force_torque_sensor/src/rq_sensor_com.cpp
+++ robotiq_force_torque_sensor/src/rq_sensor_com.cpp
@@ -1173,11 +1173,13 @@ static UINT_8 rq_com_identify_device(INT_8 const * const d_name)
 	try {
 		if (flock(fd_connexion, LOCK_EX|LOCK_NB) == -1) {
 			if (errno == EWOULDBLOCK) {
+				close(fd_connexion);
 				return 0;
 			}
 		}
 	}
 	catch (...) {
+		close(fd_connexion);
 		return 0;
 	}
 	//The serial port is open
