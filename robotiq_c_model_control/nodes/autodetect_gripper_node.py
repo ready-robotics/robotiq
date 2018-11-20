a401bd7b41377ca14c0f222f089170f4d4673e3a
Adam Calhoon <adam.calhoon@ready-robotics.com>
2018-11-20 14:04:12 -0500

    Create two nodes.
    dual_modbus_grippers.launch fires up dual_gripper_node which attempts to
    detect grippers with specific IDs (9 and 15)
    
    single_modbus_gripper.launch fires up autodetect_gripper_node attempts
    to autodetect a gripper from the list of IDs (9 or 15)

--- robotiq_c_model_control/nodes/autodetect_gripper_node.py
+++ robotiq_c_model_control/nodes/autodetect_gripper_node.py
@@ -3,13 +3,15 @@ Copyright 2018 by READY Robotics Corporation.
 All rights reserved. No person may copy, distribute, publicly display, create derivative works from or otherwise
 use or modify this software without first obtaining a license from the READY Robotics Corporation.
 """
+import rospy
+from robotiq_c_model_control.attachment_session import AttachmentSession
 from robotiq_c_model_control.constants import (
     MODBUS,
     ORIGINAL
 )
 from robotiq_c_model_control.gripper_driver import (
-    SerialGripperNode,
-    TeachmateGripperNode
+    SerialGripperDriver,
+    TeachmateGripperDriver
 )
 
 def main():
@@ -17,14 +19,20 @@ def main():
     rospy.init_node('robotiq_auto_gripper')
 
     teachmate_type = rospy.get_param('/teachmate_configuration/type', ORIGINAL)
-    node = TeachmateGripperNode() if teachmate_type == MODBUS else SerialGripperNode()
+    driver = TeachmateGripperDriver() if teachmate_type == MODBUS else SerialGripperDriver()
 
     rospy.loginfo('Attempting to autodetect a gripper')
-    if not node.autodetect(find_count=1):
-        rospy.logerr('Failed to detect a gripper')
-        exit(-1)
 
-    node.start()
+    # AttachmentSession synchronizes the launching of this attachment with
+    # ready_runtime_manager. The attachment manager will not progress until the
+    # detection is complete.
+    with AttachmentSession('/robotiq_single_gripper', 'robotiq_single_gripper'):
+        if not driver.autodetect(find_count=1):
+            rospy.logerr('Failed to detect a gripper')
+            exit(-1)
+
+        driver.start()
+
     rospy.spin()
 
 if __name__ == '__main__':
